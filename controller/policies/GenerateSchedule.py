32b0aba81c4e657dccbac69fffcfe83041a1d0b4
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-03 18:33:03 +0100

    Update GenerateSchedule.py

--- controller/policies/GenerateSchedule.py
+++ controller/policies/GenerateSchedule.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 
-# from operationalController import ControlPolicy
-class ControlPolicy:
-    pass
+from operationalController import ControlPolicy
+from controller.misc import SimulationRequest
 
 from pymoo.algorithms.soo.nonconvex.ga import GA
 from pymoo.core.problem import Problem
@@ -14,15 +13,11 @@ from pymoo.operators.crossover.ox import OrderCrossover
 from pymoo.termination.default import DefaultSingleObjectiveTermination
 
 class GenerateSchedule(ControlPolicy):
-    def fitness_function(self,orderpos):
-    
-        # from DT_Simul import digital_twin_run
-        # DataList = digital_twin_run (Nr_Of_Simul, ConwipVal, df_origine, df_tablestep, orderpos, df_proctime, destination_folder, simul_output_file_path1, simul_output_file_path2, model_path)
-        
-        df = pd.DataFrame(DataList)
-        average_data = df.mean().to_dict()
-        average_TH_value = average_data['average_TH'] # I want to optimize the TH. It represent therefore the CONTROL PARAMETER that was selected by the POLICY
-        return (average_TH_value)
+    def fitness_function(self,sequence):
+        ctrlUpdate = {"executeSchedule":{"sequence":[sequence]}}
+        req = SimulationRequest()
+        res = self.controller.dt.interface(None,ctrlUpdate,req)
+        return (res['average_TH'])
 
     class OrderOptimizationProblem(Problem):
         def __init__(self,instance,df):
@@ -33,9 +28,11 @@ class GenerateSchedule(ControlPolicy):
             n_individuals = x.shape[0]
             fitness_values = np.zeros((n_individuals, 1))
             for i in range(n_individuals):
-                order = x[i].astype(int)
-                ordered_df = self.df.iloc[order]
-                throughput = self.instance.fitness_function(ordered_df)
+                sequence = x[i].astype(int)
+                throughput = self.instance.fitness_function(sequence)
+
+                # ordered_df = self.df.iloc[order]
+                # throughput = self.instance.fitness_function(ordered_df)
                 fitness_values[i, 0] = (-1.0) * throughput
             out["F"] = fitness_values
             return fitness_values 
@@ -47,29 +44,23 @@ class GenerateSchedule(ControlPolicy):
         res = minimize(problem,algorithm,termination,seed=1,return_values_of=["F"])
 
 
-
-
-
-
-def getDT():
-    try:
-        return [el[1] for el in globals().items() if type(el[1]) is DigitalTwin][0]
-    except:
-        return
-    
-def getCTRL():
-    try:
-        return [el[1] for el in globals().items() if type(el[1]) is Controller][0]
-    except:
-        return
-
-
 if __name__ == '__main__':
+    a=SimulationRequest()
+    a[1] = 10
+    print(a)
+    a
     import pandas as pd
+    from digitaltwin import DigitalTwin
     input_path = "C:/Users/Lorenzo/Dropbox (DIG)/Ricerca/GEORGIA TECH/DTbasedcontrol/DB"
     df_orderpos = pd.read_excel(fr"{input_path}\MESb.xlsx", sheet_name="tblOrderPos")
     
     g=GenerateSchedule()
+    class Controller():
+        dt=None
+    g.controller = Controller()
+    dt = DigitalTwin()
+    dt.start()
+    g.controller.dt = dt
     g.run(df_orderpos)
     '''
     problem = OrderOptimizationProblem(df_orderpos)
