02fe37cb801928eff6c09d2afaad3decd69a0bde
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-03 18:32:57 +0100

    Create misc.py

---
+++ controller/misc.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class SimulationRequest(dict):
+    def __init__(self):
+        self['simLength'] = '01:00:00'
+        self['nrOfSimul'] = 1
+        self['output'] = ['th']
+        super().__init__()
