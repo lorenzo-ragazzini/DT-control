0934b62640ef152eb602fbca200c67201345bfe0
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-06 14:49:03 +0100

    Update smartController.py

--- controller/smartController.py
+++ controller/smartController.py
@@ -1,4 +1,11 @@
 from operationalController import OperationalController
+from sync import planned_orders
 
 class SmartController(OperationalController):
     dt = None
+    def __init__(self):
+        self.systemModel['orders'] = planned_orders()
+        n_orders = len(self.systemModel['orders'])
+        self.decisionVariables = {'sequence':list(range(1,1+n_orders))}
+        self.decisionVariables['admission'] = [False for i in range(n_orders)]
+        self.systemModel.update({'WIP':0})
