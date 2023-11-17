b7529a8d21d151ac54bfe7852a55df736a14c230
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-17 12:05:13 +0100

    implementation/dbConnect.py

---
+++ implementation/dbConnect.py
@@ -0,0 +1,81 @@
+import pyodbc
+import pandas as pd
+from time import sleep
+
+class Connection:
+	def __init__(self,tbls,path_to_file):
+		self.tbls = tbls
+		self.path_to_file = path_to_file
+	def connect(self):
+		self.conn = pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:/MES4/FestoMES.accdb;")
+		self.cur = self.conn.cursor()
+	def disconnect(self):
+		self.cur.close()
+		self.conn.close()
+	def run(self,timeout):
+		try:
+			while True:
+				dfs = self.process()
+				self.save(dfs)
+				sleep(timeout)
+		except: #disconnect in case of error
+			self.disconnect()
+	def process(self):
+		dfs = dict()
+		for tbl in ["tblStepDef","tblStep","tblOrderPos"]:
+			query = "SELECT * FROM "+tbl
+			qry = self.cur.execute(query)
+			df = pd.DataFrame(qry.fetchall())
+			cols = pd.read_sql(query, self.conn).columns
+			if df.size>0:
+				df.columns=[0]
+				df = df[0].apply(list).apply(pd.Series)
+				df.columns = cols
+			else:
+				df = pd.DataFrame(qry.fetchall(),columns=cols)
+			dfs[tbl] = df
+		return dfs
+	def save(self,dfs):
+		with pd.ExcelWriter(self.path_to_file) as writer:
+			for tbl in ["tblStepDef","tblStep","tblOrderPos"]:
+				dfs[tbl].to_excel(writer, sheet_name=tbl, index=False)
+	
+
+c=Connection(tbls=["tblStepDef","tblStep","tblOrderPos"],path_to_file='MESb.xlsx')
+c.connect()
+c.run(10)
+c.disconnect()
+
+
+if __name__ == '__main__':	
+	#pyodbc.lowercase = False
+	conn = pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:/MES4/FestoMES.accdb;")
+	cur = conn.cursor()
+	#qry = cur.execute('select * from tblStep')
+	#qry = cur.execute('select * from tblStepDef')
+	#cur.execute('select * from tblOrderPos')
+
+	dfs = dict()
+	for tbl in ["tblStepDef","tblStep","tblOrderPos"]:
+		query = "SELECT * FROM "+tbl
+		qry = cur.execute(query)
+		df = pd.DataFrame(qry.fetchall())
+		cols = pd.read_sql(query, conn).columns
+		if df.size>0:
+			df.columns=[0]
+			df = df[0].apply(list).apply(pd.Series)
+			df.columns = cols
+		else:
+			df = pd.DataFrame(qry.fetchall(),columns=cols)
+		dfs[tbl] = df
+		#df.columns=pd.read_sql(query, conn).columns
+		#df[0].to_list(), columns=pd.read_sql(query, conn).columns)
+		#dfs[query] = pd.DataFrame(qry.fetchall().str.split(','),columns=)
+	print(dfs)
+
+	with pd.ExcelWriter("MESb.xlsx") as writer:
+		for tbl in ["tblStepDef","tblStep","tblOrderPos"]:
+			dfs[tbl].to_excel(writer, sheet_name=tbl, index=False)
+
+	cur.close()
+	conn.close()
