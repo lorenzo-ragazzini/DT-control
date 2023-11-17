ece345b76df5fdfe41a0e4d34c375c71ee60d8db
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-17 10:48:20 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dt-control",
+    version="0.0.1",
+    author="Lorenzo",
+    author_email="@",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lorenzo-ragazzini/dt-control",
+    packages=setuptools.find_namespace_packages()+setuptools.find_packages(),
+    package_data={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license="MIT",
+    platforms="Python 3",
+)
