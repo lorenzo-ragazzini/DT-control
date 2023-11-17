867199a1152c6dc478bfedf14ded2de6f53b336d
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-17 20:03:25 +0100

    update smartController.py

--- DTPPC/controller/smartController.py
+++ DTPPC/controller/smartController.py
@@ -4,8 +4,8 @@ class SmartController(OperationalController):
     dt = None
     def __init__(self):
         self.systemModel['orders'] = []
-        self.decisionVariables = {'sequence':list(range(1,1+n_orders))}
-        self.decisionVariables['admission'] = [False for i in range(n_orders)]
+        self.decisionVariables = {'sequence':list(range(1,1+self.n_orders))}
+        self.decisionVariables['admission'] = [False for i in range(self.n_orders)]
         self.systemModel.update({'WIP':0})
     @property
     def n_orders(self):
