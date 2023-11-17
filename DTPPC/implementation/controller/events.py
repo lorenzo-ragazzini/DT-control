315c69aaf15693c12157cda0fb245c34b6d4a605
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-17 19:56:45 +0100

    update events.py

--- DTPPC/implementation/controller/events.py
+++ DTPPC/implementation/controller/events.py
@@ -37,20 +37,5 @@ class EventListenerMsg:
                 if event not in self.log:
                     print(event)
                     self.log.append(event)
-                    self.ctrl.send(event.content)
+                    asyncio.run(self.ctrl.send(event.content))
             await asyncio.sleep(self.timeout)
-        
-if __name__ == '__main__':
-    log = list()
-    el = EventCreator()
-    #temp
-    el.old = pd.read_excel(el.input_path+'/MESb old.xlsx',sheet_name='tblOrderPos') 
-    #
-    el.run(10)
-    while True:
-        events = el.events()
-        for event in events:
-            print(pd.Timestamp.now(),event)
-            log.append([pd.Timestamp.now(),event])
-        sleep(1)
-    
