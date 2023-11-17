4cb9880a0e28d2af7866c343ee9254dd03667e13
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-17 14:36:20 +0100

    update localUpload.py

--- implementation/local/localUpload.py
+++ implementation/local/localUpload.py
@@ -1,4 +1,4 @@
-from dtcontrol.implementation.communication.communication import ShareFile, ShareFileOnly
+from implementation.communication.communication import ShareFile, ShareFileOnly
 from .syncOrders import planned_orders
 from .inputProcessing import running_orders
 from .events import EventCreatorMsg
@@ -7,7 +7,7 @@ import asyncio
 async def upload_files(timeout):
     if 'c' not in locals().keys():
         c=Connection(tbls=["tblStepDef","tblStep","tblOrderPos"],path_to_file='MESb.xlsx')
-	c.save(c.process())
+    c.save(c.process())
     running_orders()
     u1=ShareFileOnly('','WorkInProcess.xlsx','dt-input/')
     u1.upload()
@@ -19,11 +19,11 @@ async def upload_files(timeout):
 def main():
     timeout_files = 5
     timeout_events = 1
-	c.connect()
-	asyncio.run(c.run_async(timeout=5))
-    asyncio.run(upload_files(timeout_upload))
+    c.connect()
+    asyncio.run(c.run_async(timeout=5), debug=True)
+    asyncio.run(upload_files(timeout_upload), debug=True)
     e = EventCreatorMsg('events')
-    asyncio.run(e.async_run(timeout_events))
+    asyncio.run(e.async_run(timeout_events), debug=True)
 
 if __name__ == '__main__':
     main()
