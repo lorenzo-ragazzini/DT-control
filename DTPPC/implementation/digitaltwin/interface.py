323c1e30ef333faf18be976a94ea21cbcc10c06b
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-22 18:59:53 +0100

    update interface.py

--- DTPPC/implementation/digitaltwin/interface.py
+++ DTPPC/implementation/digitaltwin/interface.py
@@ -1,35 +1,10 @@
-import numpy as np
+from DTPPC.implementation.communication.message import MessengerOnly
 
-class Interface:
-    def __init__(self):
-        pass
-    def identify(self)->int:
-        l = 'abcdefghijklmnopqrstuvwxyz'
-    def synchronize(self):
-        pass
-    def update(self):
-        pass
-    def simulate(self):
-        pass
-    def interface(self):
-        self.synchronize(taskResourceInformation)
-        self.update(controlUpdate)
-        return self.simulate(request)
-
-def send():
+class DTIFront:
     pass
-    # receive inputs
-    # upload inputs
-    # wait for results
-    # download results
-    # return results
 
-def receive():
+class DTIBack:
     pass
-    #download inputs
-    #save files
-    #execute simulation
-    #upload results
 
 if __name__ == '__main__':
-    import random
+    pass
