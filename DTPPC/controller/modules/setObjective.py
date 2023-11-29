b7c6b8b63aaf1440621f2814e97079a8949f1832
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-29 15:49:25 +0100

    update settings.json, setObjective.py and setWIP.py

--- DTPPC/controller/modules/setObjective.py
+++ DTPPC/controller/modules/setObjective.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+from time import sleep
 from DTPPC.operationalController import ControlModule
 
 class SetObjective(ControlModule):
-    def run(self):
-        pass
+    def __init__(self,timeout=600) -> None:
+        self.timeout = timeout
+    def solve(self):
+        while True:
+            sleep(self.timeout)
--- DTPPC/controller/modules/setWIP.py
+++ DTPPC/controller/modules/setWIP.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
+from time import sleep
 from DTPPC.operationalController import ControlModule
 from DTPPC.controller.misc import SimulationRequest
 import pandas as pd
 
 class SetWIP(ControlModule):
-    def run(self,**kwargs):
-        r = list()
-        for WIPlevel in range(1,10):
-            ctrlUpdate = {"executeSchedule":{"sequence":self._controller.decisionVariables['sequence']}}
-            req = SimulationRequest()
-            res = self._controller.dt.interface(None,ctrlUpdate,req)
-            r.append([WIPlevel,res['TH'],res["st"]])
-        r = pd.DataFrame(r,columns=["WIP","TH","LT"]).set_index(['WIP'])
+    def __init__(self,timeout=600) -> None:
+        self.timeout = timeout
+    def solve(self,**kwargs):
+        while True:
+            sleep(self.timeout)
+            r = list()
+            for WIPlevel in range(1,10):
+                ctrlUpdate = {"executeSchedule":{"sequence":self._controller.decisionVariables['sequence']}}
+                req = SimulationRequest()
+                res = self._controller.dt.interface(None,ctrlUpdate,req)
+                r.append([WIPlevel,res['TH'],res["st"]])
+            r = pd.DataFrame(r,columns=["WIP","TH","LT"]).set_index(['WIP'])
