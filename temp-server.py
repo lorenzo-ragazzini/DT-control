5e04dc49f7f54a917426f15494fa4dd9e1442eff
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2024-01-25 02:00:39 +0100

    fixed async on server

--- temp-server.py
+++ temp-server.py
@@ -13,7 +13,10 @@ if __name__ == "__main__":
     #     input_path = json.load(f)['input_path']
     input_path = 'C:/Users/Lorenzo/Dropbox (DIG)/Ricerca/GEORGIA TECH/DTbasedcontrol/DB'
     # asyncio.run(download('WorkInProcess.xlsx',input_path,'dt-input/',5))
-    asyncio.run(download('WorkInProcess.txt',input_path,'dt-input/',5))
+    loop = asyncio.new_event_loop()
+    asyncio.run_coroutine_threadsafe(download('WorkInProcess.txt',input_path,'dt-input/',5),loop)
     ngrok_process = subprocess.Popen(f'{"C:/Users/Lorenzo/Downloads/ngrok.exe"} http 5000', shell=True, close_fds=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     # webbrowser.open()
     serve(app, host='127.0.0.1', port=5000, expose_tracebacks=True)
+
+    
