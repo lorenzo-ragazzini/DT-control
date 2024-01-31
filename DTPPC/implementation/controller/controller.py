39c48af6512554b9cfb906fe2cb1f84e39f9fed7
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2024-01-31 11:43:53 +0100

    update controller.py

--- DTPPC/implementation/controller/controller.py
+++ DTPPC/implementation/controller/controller.py
@@ -73,7 +73,7 @@ def create_controller() -> SmartController:
     ctrl.policies = [ExecuteSchedule(), ReleaseOne(WIPlimit=5), GenerateSchedule(), SetWIP(), FIFODispatchingRule(), BottleneckPrediction(), SetObjective()]
     ctrl.linkPolicies()
     ctrl.map = ControlMap()
-    ctrl.map.rules = [Rule1(), Rule2bis(), Rule3(), Rule4(), Rule5()]
+    ctrl.map.rules = [Rule1(), Rule2(), Rule3(), Rule4(), Rule5()]
     ctrl.linkRules()
     return ctrl
 
