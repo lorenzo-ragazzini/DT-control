9be0ce77d6effbf306f3248d0fbf2d73ba7609a3
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2024-02-01 15:37:09 +0100

    Update app for debugging

--- DTPPC/implementation/flask/app.py
+++ DTPPC/implementation/flask/app.py
@@ -13,17 +13,21 @@ app.results : dict[str,Any] = dict()
 
 @app.route('/new',methods=['GET'])
 def new():
-    return app.dt.new()
+    DTname = app.dt.new()
+    print('Creating DT instance: %s' %DTname)
+    return DTname
     
 @app.route('/clear',methods=['POST'])
 def clear():
-    name = request.json['name']
-    return app.dt.clear(name)
+    DTname = request.json['name']
+    print('Clearing DT instance: %s' %DTname)
+    return app.dt.clear(DTname)
     
 @app.route('/run',methods=['GET','POST'])
 def run():
     input = json.loads(request.json)
     name = input.pop('name')
+    print("Executing request %s on DT %s"%(input,name))
     if request.method == 'POST':
         app.results[name] = app.dt[name].interface(**input)
         return app.results[name]
--- DTPPC/implementation/flask/front.py
+++ DTPPC/implementation/flask/front.py
@@ -13,12 +13,12 @@ class DTInterface:
         data = {'name': name}
         requests.post(self.url+'/clear',json=data)
     def interface(self,name,taskResourceInformation,controlUpdate,request):
-        if self._debug:
-            print("Sending request to DT: %s" % request)
         headers = {'Content-Type': 'application/json'}
         if type(controlUpdate['ExecuteSchedule']['sequence']) is dict:
             controlUpdate['ExecuteSchedule']['sequence'] = [controlUpdate['ExecuteSchedule']['sequence'][key] for key in taskResourceInformation['Order'].values()]
         inputs = json.dumps({"name":name, "taskResourceInformation":taskResourceInformation, "controlUpdate":controlUpdate, "request":request})
+        if self._debug:
+            print("Sending request to DT: %s" %inputs)
         res = requests.post(self.url+'/run', json=inputs, headers=headers)
         # res = requests.get(self.url+'/run',json=inputs,headers=headers) # inutile, ottieni lo stesso risultato
         return json.loads(res.content.decode())
