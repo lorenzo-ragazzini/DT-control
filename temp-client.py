20dd68f223cfedc780b9b8db38a7259293ceb362
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2024-02-02 10:06:28 +0100

    update temp-client.py and temp-server.py

--- temp-client.py
+++ temp-client.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     if not debug:
         address="http://127.0.0.1:5000"
     else:
-        address="https://1eeb-131-175-147-135.ngrok-free.app" #without final /; if error, wait after starting the server
+        address="https://c276-131-175-147-135.ngrok-free.app" #without final /; if error, wait after starting the server
 
     dt = DTInterface(address) # interface with the DT
     dbc = DBReader(output_file=db_file) # read the ACCDB defined in DBReader class, write db_file
--- temp-server.py
+++ temp-server.py
@@ -8,6 +8,7 @@ import subprocess
 import webbrowser
   
 if __name__ == "__main__":
+    port = 5000
     app.dt = DigitalTwin()
     # with open('config.json') as f:
     #     input_path = json.load(f)['input_path']
@@ -16,11 +17,14 @@ if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.run_coroutine_threadsafe(download('WorkInProcess.txt',input_path,'dt-input/',5),loop)
     # cannot launch ngrok from here:
+    command = 'ngrok http %s' %port
+    ngrok_process = subprocess.run(["cmd", "/c", "start", "cmd", "/k", command], shell=True)
     # ngrok_process = subprocess.Popen(f'{"C:/Users/Lorenzo/Downloads/ngrok.exe"} http 5000', shell=True, close_fds=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     # webbrowser.open()
-    serve(app, host='127.0.0.1', port=5000, expose_tracebacks=True)
+    serve(app, host='127.0.0.1', port=port, expose_tracebacks=True)
 
 # the address is available at: (account Google @polimi)
 # https://dashboard.ngrok.com/cloud-edge/endpoints
     
 # api key: 2bjC06hzYIGYU6JXLeMaFByjLAL_34xbAoZM7S7XYjup2Y7tm
+# token 2YwwqqdkgiysCtMBJIY5slj6h9y_3Mgs6keY1Q8qq4tzPnDnw
