536ff82ea75f4e317a28fdcd489b112524f99eb9
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2024-05-30 16:11:47 +0200

    update popup.py

--- DTPPC/implementation/popup.py
+++ DTPPC/implementation/popup.py
@@ -1,18 +1,24 @@
+import sys
+import platform
 import threading
+import time
 import tkinter as tk
 from plyer import notification
+from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import QThread, pyqtSignal
+from multiprocessing import Process, Queue
 
-def create_self_closing_popup(title, message, image=None, duration=5):
+def create_self_closing_popup(title, message, image=None, duration=5, window_width=300, window_height=200, position_right=-1, position_top=-1):
     root = tk.Tk()
     # Set window title
     root.title(title)
     # Center the message
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    window_width = 300
-    window_height = 200
-    position_top = int(screen_height / 2 - window_height / 2)
-    position_right = int(screen_width / 2 - window_width / 2)
+    if position_right == -1:
+        screen_width = root.winfo_screenwidth()
+        position_right = int(screen_width / 2 - window_width / 2)
+    if position_top == -1:
+        screen_height = root.winfo_screenheight()
+        position_top = int(screen_height / 2 - window_height / 2)
     root.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
     # Create a frame with a slight relief and a border
     frame = tk.Frame(root, relief='raised', borderwidth=2, bg='white')
@@ -32,6 +38,7 @@ def create_self_closing_popup(title, message, image=None, duration=5):
     # Close the window after 'duration' seconds
     root.after(duration * 1000, root.destroy)
     root.mainloop()
+    return root 
     
 def popup(title, message="", image=None, timeout=5):
     threading.Thread(target=create_self_closing_popup, args=(title, message, image, timeout)).start()
@@ -39,8 +46,58 @@ def popup(title, message="", image=None, timeout=5):
 def notify(title, message=" ", timeout=2):
     notification.notify(title=title, message=message, app_name='DTPPC', timeout=5)
 
+if platform.system() == 'Windows' and platform.release() == '7':
+    class MessageThread(QThread):
+        message_signal = pyqtSignal(str)
+        def __init__(self, queue):
+            super().__init__()
+            self.queue = queue
+        def run(self):
+            while True:
+                message = self.queue.get()
+                self.message_signal.emit(message)
+    class AppDemo(QWidget):
+        def __init__(self, queue):
+            super().__init__()
+            self.messages = []
+            self.layout = QVBoxLayout()
+            self.message_thread = MessageThread(queue)
+            self.message_thread.message_signal.connect(self.add_message)
+            self.message_thread.start()
+            self.setLayout(self.layout)
+        def add_message(self, message):
+            self.messages.insert(0, QLabel(message))
+            if len(self.messages) > 20:
+                self.messages.pop().deleteLater()
+            for i in range(len(self.messages)):
+                self.layout.insertWidget(i, self.messages[i])
+    class Logger:
+        def __init__(self):
+            self.queue = Queue()
+            p = Process(target=self.run_app, args=(self.queue,))
+            p.start()
+        def run_app(self,queue):
+            app = QApplication(sys.argv)
+            demo = AppDemo(queue)
+            demo.show()
+            sys.exit(app.exec_())
+
+        def notify(self,title, message, timeout=None):
+            msg = "%s: %s"%(title, message)
+            self.queue.put(msg)
+    
+    logger = Logger()
+    notify = logger.notify
+
 if __name__ == '__main__':
     input = "ewrwe 34534 fsd \nerewr ere er"
     name = "r3r34werwwe"
-    popup(title="Digital Twin request",message="DT instance: %s\nis executing request:\n%s"%(name,input),timeout=5)
-    notify(title="Digital Twin request",message="DT instance: %s\nis executing request:\n%s"%(name,input),timeout=5)
+    # root = notify(title="Digital Twin request",message="DT instance: %s\nis executing request:\n%s"%(name,input),timeout=5)
+    # notify(title="Digital Twin request",message="DT instance: %s\nis executing request:\n%s"%(name,input),timeout=5)
+    window_height, window_width, position_right, position_top = 200, 300, 0, 0
+
+    logger = Logger()
+    notify = logger.notify
+    notify("1")
+    
+    
