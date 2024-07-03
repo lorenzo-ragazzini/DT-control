80ad1e6e3512842f82fa2cdb23e6a5ea119e8cf0
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2024-07-03 11:34:46 +0200

    update front.py

--- DTPPC/implementation/flask/front.py
+++ DTPPC/implementation/flask/front.py
@@ -8,18 +8,24 @@ class DTInterface:
             self.url = url
         self._debug = debug
     def new(self) -> str:
-        return requests.get(self.url+'/new').content.decode()
+        try:
+            return requests.get(self.url+'/new').content.decode()
+        except ConnectionError:
+            print("Connection error")
     def clear(self,name:str=None):
         data = {'name': name}
         requests.post(self.url+'/clear',json=data)
     def interface(self,name,taskResourceInformation,controlUpdate,request):
-        headers = {'Content-Type': 'application/json'}
-        if type(controlUpdate['ExecuteSchedule']['sequence']) is dict:
-            controlUpdate['ExecuteSchedule']['sequence'] = [controlUpdate['ExecuteSchedule']['sequence'][key] for key in taskResourceInformation['Order'].values() if key in controlUpdate['ExecuteSchedule']['sequence'].keys()]
-        inputs = json.dumps({"name":name, "taskResourceInformation":taskResourceInformation, "controlUpdate":controlUpdate, "request":request})
-        if self._debug:
-            print("Sending request to DT: %s" %inputs)
-        res = requests.post(self.url+'/run', json=inputs, headers=headers)
-        # res = requests.get(self.url+'/run',json=inputs,headers=headers) # inutile, ottieni lo stesso risultato
-        return json.loads(res.content.decode())
+        try:
+            headers = {'Content-Type': 'application/json'}
+            if type(controlUpdate['ExecuteSchedule']['sequence']) is dict:
+                controlUpdate['ExecuteSchedule']['sequence'] = [controlUpdate['ExecuteSchedule']['sequence'][key] for key in taskResourceInformation['Order'].values() if key in controlUpdate['ExecuteSchedule']['sequence'].keys()]
+            inputs = json.dumps({"name":name, "taskResourceInformation":taskResourceInformation, "controlUpdate":controlUpdate, "request":request})
+            if self._debug:
+                print("Sending request to DT: %s" %inputs)
+            res = requests.post(self.url+'/run', json=inputs, headers=headers)
+            # res = requests.get(self.url+'/run',json=inputs,headers=headers) # inutile, ottieni lo stesso risultato
+            return json.loads(res.content.decode())
+        except ConnectionError:
+            print("Connection error")
         
